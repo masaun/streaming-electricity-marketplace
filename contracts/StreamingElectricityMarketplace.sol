018a259b2fd458d3bfd65deff7f95c55b6c7c259
masaun <masanoriuno@gmail.com>
2021-01-11 21:39:45 +0900

    Define the Device struct

--- contracts/StreamingElectricityMarketplace.sol
+++ contracts/StreamingElectricityMarketplace.sol
@@ -2,9 +2,17 @@ pragma solidity ^0.5.16;
 
 
 /**
- * @notice - This is the StreamingElectricityMarketplace contract
+ * @notice - This is the StreamingElectricityMarketplace contract for M2M supscription payment of electricity.
  */
 contract StreamingElectricityMarketplace {
+
+    struct Device {  /// [Key]: Array index is currentDeviceId - 1
+        address ownerAddress;
+        address deviceAddress;
+        uint sellPrice;  /// [Note]: Threshold of price for selling
+        uint buyPrice;   /// [Note]: Threshold of price for buying 
+    }
+    Device[] devices;
 
     constructor() public {}
 
