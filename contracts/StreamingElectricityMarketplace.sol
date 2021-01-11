816cabe86d61cd403fb28bafd57122aa5c840a0b
masaun <masanoriuno@gmail.com>
2021-01-11 21:28:25 +0900

    Define StreamingElectricityMarketplace.sol

---
+++ contracts/StreamingElectricityMarketplace.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.16;
+
+
+/**
+ * @notice - This is the StreamingElectricityMarketplace contract
+ */
+contract StreamingElectricityMarketplace {
+
+    constructor() public {}
+
+}
