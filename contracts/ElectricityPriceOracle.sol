56b60d33711b761f4a4b56650ecedf252aa0d879
masaun <masanoriuno@gmail.com>
2021-01-16 21:19:14 +0900

    Replace OAR with '0x1215040e5aB78f4D25CAa8A04F81A42F8EA012cd' for ethereum-bridge

--- contracts/ElectricityPriceOracle.sol
+++ contracts/ElectricityPriceOracle.sol
@@ -35,7 +35,8 @@ contract ElectricityPriceOracle is usingOraclize {
         // send some Ethers with the deployment!
 
         // this line is given by the ethereum-bridge
-        OAR = OraclizeAddrResolverI(0xBb029d89EFb9E7EDA9C8BAEb164cfBd021A6aa77);
+        OAR = OraclizeAddrResolverI(0x1215040e5aB78f4D25CAa8A04F81A42F8EA012cd);    /// New (ethereum-bridge)
+        //OAR = OraclizeAddrResolverI(0xBb029d89EFb9E7EDA9C8BAEb164cfBd021A6aa77);  /// Original
 
         // first check at contract creation
         updateElectric();
