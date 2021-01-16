94197fe801d60d825cf3003c66bfa5043569a09f
masaun <masanoriuno@gmail.com>
2021-01-16 11:44:50 +0900

    Rename import instance name with 'ElectricityPriceOracle'

--- test/test-local/ElectricityPriceOracle.test.js
+++ test/test-local/ElectricityPriceOracle.test.js
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
 
 /// Artifact of the ElectricityPriceOracle.test contract 
-const ElectricityPriceOracle = artifacts.require("ElectricityPriceOracle.test");
+const ElectricityPriceOracle = artifacts.require("ElectricityPriceOracle");
 
 /// Global variable
 let electricityPriceOracle;
